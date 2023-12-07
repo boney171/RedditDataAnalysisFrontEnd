f7d9b9ff5665b169c44c201d57d767e5120fcba5
Ubuntu <ubuntu@ip-172-31-46-11.us-east-2.compute.internal>
2023-12-07 06:19:34 +0000

    Set STATIC_ROOT in settings

--- redditanalysis/settings.py
+++ redditanalysis/settings.py
@@ -133,6 +133,9 @@ CORS_ALLOWED_ORIGINS = [
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
